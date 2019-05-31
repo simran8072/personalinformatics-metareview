3076c6a55d32fe122b7f0b538e385f1bfd520d97
chessegg <43458707+chessegg@users.noreply.github.com>
2019-05-31 13:10:09 -0700

    5/31
    changed name of updated list of papers from no_duplicates to list_of_papers. Took out articles that were extended abstracts, adjunct proceedings, or part of workshops. The deleted list of articles is in deleted_articles.csv

--- read_IDs.py
+++ read_IDs.py
@@ -20,7 +20,8 @@ for id in list_of_IDs:
 #print(counter) #prints out total number of articles at the end
 #644 articles in combined_file; after removing duplicates, 472 total articles.
 
-with open('no_duplicates.csv', 'w', newline='') as writeFile:
+#now, write the info into the list_of_papers csv file
+with open('list_of_papers.csv', 'w', newline='') as writeFile:
     fieldnames = ['type', 'id', 'author', 'editor', 'advisor', 'note', 'title', 'pages', 'article_no',
                   'num_pages', 'keywords', 'doi', 'journal', 'issue_date', 'volume', 'issue_no',
                   'description', 'month', 'year', 'issn', 'booktitle', 'acronym', 'edition', 'isbn',
@@ -30,7 +31,9 @@ with open('no_duplicates.csv', 'w', newline='') as writeFile:
     writer.writeheader()
     
     #Need to iterate through list_of_IDs, find the matching ID in combined_file.csv using the
-    #reader, then write that to the no_duplicates.csv file.
+    #reader, then write that to the list_of_papers.csv file. Don't include articles that have the words
+    #'Adjunct' or 'Extended' or 'Workshop' in the booktitle, because those are for adjunct proceedings or
+    #extended abstracts or workshops, not full length research papers
     with open('combined_file.csv', newline='') as csvfile:
         reader = csv.DictReader(csvfile)
         #go through each id in the list of non-duplicates and each row of the combined file
@@ -39,9 +42,11 @@ with open('no_duplicates.csv', 'w', newline='') as writeFile:
              
                 # if we found the record we are looking for
                 if (idnum == row['id']):
-                    
+                    if ('Adjunct' in row['booktitle'] or 'Extended' in row['booktitle'] or 'Workshop' in row['booktitle']):
+                        break;
+                    else:
                     # write out the record
-                    writer.writerow({'type': row['type'], 'id': row['id'], 'author': row['author'],
+                        writer.writerow({'type': row['type'], 'id': row['id'], 'author': row['author'],
                                      'editor': row['editor'],'advisor': row['advisor'], 'note': row['note'],
                                      'title': row['title'], 'pages': row['pages'],'article_no': row['article_no'],
                                      'num_pages': row['num_pages'], 'keywords': row['keywords'],'doi': row['doi'],
@@ -52,7 +57,35 @@ with open('no_duplicates.csv', 'w', newline='') as writeFile:
                                      'conf_loc': row['conf_loc'], 'publisher': row['publisher'],
                                      'publisher_loc': row['publisher_loc']})
                     
-                    # move on to the next ID
-                    break
+                        # move on to the next ID
+                        break
+
+
+#writing the list of deleted articles to deleted_articles.csv
+with open('deleted_articles.csv', 'w', newline='') as writeDeletedFiles:
+    fieldnames = ['type', 'id', 'author', 'editor', 'advisor', 'note', 'title', 'pages', 'article_no',
+                  'num_pages', 'keywords', 'doi', 'journal', 'issue_date', 'volume', 'issue_no',
+                  'description', 'month', 'year', 'issn', 'booktitle', 'acronym', 'edition', 'isbn',
+                  'conf_loc', 'publisher', 'publisher_loc']
+    writer = csv.DictWriter(writeDeletedFiles, fieldnames = fieldnames)
+    
+    writer.writeheader()
+    with open('includes adjunct, extended, workshop.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            if ('Adjunct' in row['booktitle'] or 'Extended' in row['booktitle'] or 'Workshop' in row['booktitle']):
+                writer.writerow({'type': row['type'], 'id': row['id'], 'author': row['author'],
+                                'editor': row['editor'],'advisor': row['advisor'], 'note': row['note'],
+                                'title': row['title'], 'pages': row['pages'],'article_no': row['article_no'],
+                                'num_pages': row['num_pages'], 'keywords': row['keywords'],'doi': row['doi'],
+                                'journal': row['journal'], 'issue_date': row['issue_date'],'volume': row['volume'],
+                                'issue_no': row['issue_no'], 'description': row['description'],'month': row['month'],
+                                'year': row['year'], 'issn': row['issn'], 'booktitle': row['booktitle'],
+                                'acronym': row['acronym'], 'edition': row['edition'], 'isbn': row['isbn'],
+                                'conf_loc': row['conf_loc'], 'publisher': row['publisher'],
+                                'publisher_loc': row['publisher_loc']})
+                    
+                
+
 
 
