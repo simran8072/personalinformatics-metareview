8c91daf5d0e3fe5269a64069d2bfe94ce8966989
chessegg <43458707+chessegg@users.noreply.github.com>
2019-06-03 15:19:00 -0700

    6/3
    Full list of papers to go through in list_of_papers

--- read_write_csvfiles.py
+++ read_write_csvfiles.py
@@ -1,4 +1,5 @@
 import csv
+import re
 
 counter = 0 #will keep track of the total number of articles
 list_of_IDs = []
@@ -22,7 +23,7 @@ for id in list_of_IDs:
 
 #now, write the info into the list_of_papers csv file
 with open('list_of_papers.csv', 'w', newline='') as writeFile:
-    fieldnames = ['type', 'id', 'author', 'editor', 'advisor', 'note', 'title', 'pages', 'article_no',
+    fieldnames = ['type', 'id', 'URL', 'author', 'editor', 'advisor', 'note', 'title', 'pages', 'article_no',
                   'num_pages', 'keywords', 'doi', 'journal', 'issue_date', 'volume', 'issue_no',
                   'description', 'month', 'year', 'issn', 'booktitle', 'acronym', 'edition', 'isbn',
                   'conf_loc', 'publisher', 'publisher_loc']
@@ -47,7 +48,7 @@ with open('list_of_papers.csv', 'w', newline='') as writeFile:
                         break;
                     else:
                     # write out the record
-                        writer.writerow({'type': row['type'], 'id': row['id'], 'author': row['author'],
+                        writer.writerow({'type': row['type'], 'id': row['id'], 'URL': "https://dl.acm.org/citation.cfm?id=" + row['id'], 'author': row['author'],
                                      'editor': row['editor'],'advisor': row['advisor'], 'note': row['note'],
                                      'title': row['title'], 'pages': row['pages'],'article_no': row['article_no'],
                                      'num_pages': row['num_pages'], 'keywords': row['keywords'],'doi': row['doi'],
@@ -64,7 +65,7 @@ with open('list_of_papers.csv', 'w', newline='') as writeFile:
 
 #writing the list of deleted articles to deleted_articles.csv
 with open('deleted_articles.csv', 'w', newline='') as writeDeletedFiles:
-    fieldnames = ['type', 'id', 'author', 'editor', 'advisor', 'note', 'title', 'pages', 'article_no',
+    fieldnames = ['type', 'id', 'URL', 'author', 'editor', 'advisor', 'note', 'title', 'pages', 'article_no',
                   'num_pages', 'keywords', 'doi', 'journal', 'issue_date', 'volume', 'issue_no',
                   'description', 'month', 'year', 'issn', 'booktitle', 'acronym', 'edition', 'isbn',
                   'conf_loc', 'publisher', 'publisher_loc']
@@ -76,7 +77,7 @@ with open('deleted_articles.csv', 'w', newline='') as writeDeletedFiles:
         for row in reader:
             if ('Adjunct' in row['booktitle'] or 'Extended' in row['booktitle'] or 'Workshop' in row['booktitle']
                 or 'Companion' in row['booktitle'] or 'person tracking' in row['keywords']):
-                writer.writerow({'type': row['type'], 'id': row['id'], 'author': row['author'],
+                writer.writerow({'type': row['type'], 'id': row['id'], 'URL': "https://dl.acm.org/citation.cfm?id=" + row['id'], 'author': row['author'],
                                 'editor': row['editor'],'advisor': row['advisor'], 'note': row['note'],
                                 'title': row['title'], 'pages': row['pages'],'article_no': row['article_no'],
                                 'num_pages': row['num_pages'], 'keywords': row['keywords'],'doi': row['doi'],
@@ -93,7 +94,63 @@ with open('deleted_articles.csv', 'w', newline='') as writeDeletedFiles:
 #of papers that cite these papers, but no way to do a csv download. I can hover over the hyperlink and
 #see the ID of each paper (and IDs are unique). So, I could extract that info from the webpage somehow,
 #possibly web scraping(?), and then check if the ID matches any ID in the current list of papers. If there
-#is a match, then skip to the next ID. If not, then go into the csv file containing a list of pretty much
-#all papers on the website (I can create this by doing a search for a word like 'a' or 'the' and then
-#downloading the csv file)and search for the paper by its ID, and add it to the current papers list.
+#is a match, then skip to the next ID. If not, then go into the csv file containing a list of all papers
+#on the website and search for the paper by its ID, and add it to the current papers list.
+                
+#get the html file                
+with open('paper1.html', 'r') as file:
+    data = file.read()
+#extract the ID and article data using regular expressions
+tuples_one = re.findall(r'a href="https:\/\/dl\.acm\.org\/citation\.cfm\?id=(\d+)">\n(.+)\n<\/a>', data)
 
+with open('paper2.html', 'r') as file:
+    data = file.read()
+tuples_two = re.findall(r'a href="https:\/\/dl\.acm\.org\/citation\.cfm\?id=(\d+)">\n(.+)\n<\/a>', data)
+
+with open('paper3.html', 'r') as file:
+    data = file.read()
+tuples_three = re.findall(r'a href="https:\/\/dl\.acm\.org\/citation\.cfm\?id=(\d+)">\n(.+)\n<\/a>', data)
+
+#combine into one list of tuples
+combined_tuples = tuples_one + tuples_two + tuples_three
+
+#print(combined_tuples)
+
+with open('list_of_papers.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    
+    with open('cites_key_articles', 'w', newline='') as writeNewFile:
+        fieldnames = ['type', 'id', 'URL', 'author']
+        writer = csv.DictWriter(writeNewFile, fieldnames = fieldnames)
+        writer.writeheader()
+        counter = 0
+        for tuple in combined_tuples:
+            #add_row = True
+            if ((tuple[0] in list_of_IDs or 'Adjunct' in tuple[1] or 'Extended' in tuple[1] or 'Workshop' in tuple[1])):
+                continue
+            #rowsLookedAt = 0
+            #for row in reader:
+            #    rowsLookedAt += 1
+            #    if (tuple[0] == row['id']):
+                    #counter += 1
+            #        add_row = False
+            #        break
+            #print(rowsLookedAt)
+            #if (add_row == True):    
+            writer.writerow({'type': "article", 'id': tuple[0],
+                             'URL': "https://dl.acm.org/citation.cfm?id=" + tuple[0],'author': tuple[1]})
+                
+        
+#with open('list_of_papers', 'a') as writeNewFile:
+    #fieldnames = ['type', 'id', 'URL', 'author']
+#    writer = csv.writer(writeNewFile)
+#    
+#    with open('cites_key_articles.csv', newline='') as csvfile:
+#        reader = csv.DictReader(csvfile)
+#        row = [row['type'], row['id'], row['URL'],row['author']]
+        
+#        for row in reader:
+#            writer.writerow(row)
+                
+
+    
